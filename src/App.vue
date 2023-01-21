bded878ddef41d477b01aa289d25a43ff1f96d0c
termokop <termokop564@gmail.com>
2023-01-21 22:48:14 +0400

    add component GraphForCoin

--- src/App.vue
+++ src/App.vue
@@ -82,50 +82,15 @@
       </dl>
       <hr class="w-full border-t border-gray-600 my-4" />
     </template>
-    <section v-if="selected" class="relative">
-      <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-        {{ selected.name }} - USD
-      </h3>
-      <div 
-        class="flex items-end border-gray-600 border-b border-l h-64" 
-        ref="graph">
-        <div
-          v-for="(bar, index) in normalizedGraph"
-          ref="grahElement"
-          :key="index"
-          :style="{height: `${bar}%`}"
-          class="bg-purple-800 border w-10"
-        ></div>
 
-      </div>
-      <button
-        @click="selected = null"
-        type="button"
-        class="absolute top-0 right-0"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          xmlns:xlink="http://www.w3.org/1999/xlink"
-          xmlns:svgjs="http://svgjs.com/svgjs"
-          version="1.1"
-          width="30"
-          height="30"
-          x="0"
-          y="0"
-          viewBox="0 0 511.76 511.76"
-          style="enable-background:new 0 0 512 512"
-          xml:space="preserve"
-        >
-          <g>
-            <path
-              d="M436.896,74.869c-99.84-99.819-262.208-99.819-362.048,0c-99.797,99.819-99.797,262.229,0,362.048    c49.92,49.899,115.477,74.837,181.035,74.837s131.093-24.939,181.013-74.837C536.715,337.099,536.715,174.688,436.896,74.869z     M361.461,331.317c8.341,8.341,8.341,21.824,0,30.165c-4.16,4.16-9.621,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    l-75.413-75.435l-75.392,75.413c-4.181,4.16-9.643,6.251-15.083,6.251c-5.461,0-10.923-2.091-15.083-6.251    c-8.341-8.341-8.341-21.845,0-30.165l75.392-75.413l-75.413-75.413c-8.341-8.341-8.341-21.845,0-30.165    c8.32-8.341,21.824-8.341,30.165,0l75.413,75.413l75.413-75.413c8.341-8.341,21.824-8.341,30.165,0    c8.341,8.32,8.341,21.824,0,30.165l-75.413,75.413L361.461,331.317z"
-              fill="#718096"
-              data-original="#000000"
-            ></path>
-          </g>
-        </svg>
-      </button>
-    </section>
+    <graph-for-coin 
+      v-if="selected" 
+      :selected="selected"
+      :graphProp="graph"
+      class="relative"
+      @close-graph="selected = null"
+      />
+  
   </div>
 </div>
 
@@ -135,11 +100,13 @@
 
 import {subscribeToTicker, unsubscribeFromTicker, invalidTickers } from './api'
 import AddTicker from "./components/AddTicker.vue"
+import GraphForCoin from "./components/GraphForCoin.vue"
 
 export default {
   name: 'App',
   components: {
-    AddTicker
+    AddTicker,
+    GraphForCoin,
   },
   data() {
     return {
@@ -150,8 +117,6 @@ export default {
 
       selected: null,
       graph: [],
-      maxGraphElems: 1,
-      widthFraphElement: 1,
 
       coin_list: [],
 
@@ -162,13 +127,7 @@ export default {
     }
   },
   methods: {
-    calculateMaxGraphElements() {
-      if(!this.$refs.graph) return
-      this.maxGraphElems = this.$refs.graph.clientWidth / this.widthFraphElement
-      while(this.graph.length > this.maxGraphElems) {
-            this.graph.shift()
-      }
-    },
+
     updateTicker(tickerName, price) {
       this.tickers
       .filter(t => t.name === tickerName)  
@@ -178,8 +137,6 @@ export default {
           if(this.graph.length > this.maxGraphElems) {
             this.graph.shift()
           }
-          this.widthFraphElement = this.$refs.grahElement ? this.$refs.grahElement[0].clientWidth : 0
-          this.calculateMaxGraphElements()
         }
         t.price = price
       })
@@ -229,19 +186,8 @@ export default {
     paginatedTickers() {
       return this.filteredTickers.slice(this.startIndex, this.endIndex)
     },
-    
     hasNextPage() {
       return this.filteredTickers.length > this.endIndex
-    },
-    normalizedGraph() {
-      const maxPrice = Math.max(...this.graph)
-      const minPrice = Math.min(...this.graph)
-
-      if(minPrice === maxPrice) return this.graph.map(() => 50)
-
-      return this.graph.map(
-        currentPrice => 5 + ((currentPrice - minPrice) * 95) / (maxPrice - minPrice) 
-      )
     },
     pageStateOptions() {
       return {
@@ -269,7 +215,7 @@ export default {
         subscribeToTicker(ticker.name, newPrice => this.updateTicker(ticker.name, newPrice))
       })
     }
-    setInterval(this.updateTickers, 5000)
+    // setInterval(this.updateTickers, 5000)
 
     const takeCoinList = async() => {
             this.isShowLoader = true
@@ -280,12 +226,6 @@ export default {
     }
     takeCoinList()
 
-  },
-  mounted() {
-    window.addEventListener("resize", this.calculateMaxGraphElements)
-  },
-  beforeUnmount() {
-    window.removeEventListener("resize", this.calculateMaxGraphElements)
   },
   watch: {
     tickers() {
